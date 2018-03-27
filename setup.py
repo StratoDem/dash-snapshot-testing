bac4680db0d2a038247e254b71b3d6201e1094c9
michael <michael@stratodem.com>
2018-03-27 09:42:20 -0400

    Ignore venv, update LICENSE to MIT

--- setup.py
+++ setup.py
@@ -13,13 +13,15 @@ from setuptools import setup
 
 setup(
     name='dash-snapshot-testing',
-    version='1.0.0',
+    version='1.0.1',
     author='Michael Clawar, Eric Linden',
     author_email='tech@stratodem.com',
     packages=['snapshot_test'],
-    license='(c) 2018 StratoDem Analytics. All rights reserved.',
+    license='MIT',
     description='Dash snapshot testing package',
     url='https://github.com/StratoDem/dash-snapshot-testing',
+    keywords='dash snapshot plotly testing unittest',
+    python_requires='>=3',
     install_requires=[
         'dash>=0.19.0',
         'plotly>=2.2.3',
